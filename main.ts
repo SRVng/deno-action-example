ed9203a90f7f29646131c6b476b13f16a9b82ca2
Saravut Nakglom <snakglom@gmail.com>
2023-02-02 00:49:14 +0700

    Change path

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
-import { Octokit } from 'npm:@octokit/rest@x.x.x';
+import { Octokit } from "npm:@octokit/rest@x.x.x";
 
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const octokit = new Octokit({ auth: Deno.env.get('GITHUB_TOKEN' )});
-  const { data } = await octokit.repos.get({ owner: 'SRVngOrg', repo: 'test-cicd' });
+  const octokit = new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") });
+  const { data } = await octokit.repos.get({ owner: "denoland", repo: "deno" });
 
-  console.log("Add 2 + 3 =", add(2, 3));
   console.log(data.clone_url);
 }
